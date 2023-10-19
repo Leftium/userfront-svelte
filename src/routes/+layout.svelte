dbcd51c05cb19cd322957751910f1bc8040c9d1b
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-10-20 07:30:14 +0900

    Add simple nav and basic routes.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/login">Login</a></li>
+		<li><a href="/reset">Reset</a></li>
+		<li><a href="/dashboard">Dashboard</a></li>
+	</ul>
+</nav>
+
+<slot />
