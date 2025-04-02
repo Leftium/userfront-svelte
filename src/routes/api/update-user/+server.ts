394dd3e546801ebac70ff7c329b3990e6eaccac3
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-04-02 17:30:55 +0900

    Add UserfrontApi and simplify update-user api route.

--- src/routes/api/update-user/+server.ts
+++ src/routes/api/update-user/+server.ts
@@ -1,7 +1,10 @@
 import { USERFRONT_API_KEY } from '$env/static/private';
 import { PUBLIC_USERFRONT_PUBLIC_KEY } from '$env/static/public';
-import { verifyToken } from '$lib/index.js';
 import { error, json } from '@sveltejs/kit';
+import { verifyToken } from '$lib/index.js';
+import { UserfrontApi } from '$lib/userfront-api';
+
+const userfrontApi = new UserfrontApi(USERFRONT_API_KEY);
 
 export const PUT = async ({ request }) => {
 	const payload = await request.json();
@@ -16,24 +19,14 @@ export const PUT = async ({ request }) => {
 	// Verify the token using the Userfront public key:
 	const result = verifyToken(PUBLIC_USERFRONT_PUBLIC_KEY, token);
 	if (result.isErr()) {
-		error(403, 'Error validating authorization token.');
+		error(403, `Error validating authorization token. (${result.error})`);
 	}
 
 	const userId = result.value.userId;
 
-	const response = await fetch(`https://api.userfront.com/v0/users/${userId}`, {
-		method: 'PUT',
-		headers: {
-			'content-type': 'application/json',
-			authorization: `Bearer ${USERFRONT_API_KEY}`
-		},
-		body: JSON.stringify(payload)
+	const results = await userfrontApi.PUT(`/users/${userId}`, {
+		body: payload
 	});
-
-	console.log(response.status);
-	console.log(response.statusText);
-
-	const results = await response.json();
 	console.log(results);
 
 	return json(results);
