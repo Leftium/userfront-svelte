60cfcb7b1d6ed8246c31499b2f20881f370d6e31
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-04-02 11:28:19 +0900

    Move handleMissingAuthGuard() to $lib.

---
+++ src/lib/sveltekit/handle-missing-auth-guard.ts
@@ -0,0 +1,21 @@
+import { error, type Handle } from '@sveltejs/kit';
+
+const protectedRouteRegex = /\/\(protected\)/;
+
+export const handleMissingAuthGuard: Handle = async ({ event, resolve }) => {
+	const isProtectedRoute = event.route.id && protectedRouteRegex.test(event.route.id);
+
+	console.log(`HANDLE          : ${event.route.id}`);
+	console.log(`isProtectedRoute: ${isProtectedRoute}`);
+	console.log(`isDataRequest   : ${event.isDataRequest}`);
+
+	const resolved = isProtectedRoute ? await resolve(event) : await resolve(event);
+
+	console.log(`hasAuthGuard: ${event.locals.hasAuthGuard}`);
+
+	if (isProtectedRoute && !event.locals.hasAuthGuard) {
+		error(400, `Protected route is missing an auth guard. (${event.route.id})`);
+	}
+
+	return resolved;
+};
